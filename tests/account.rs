b03865260a9f54deda7159602bcdde7fbedccd5b
Ryder <ryder@Ryders-MBP.attlocal.net>
2022-11-24 16:05:02 -0800

    Testing fixes

--- tests/account.rs
+++ tests/account.rs
@@ -2,9 +2,14 @@ use dash_rs::request;
 
 #[tokio::test]
 async fn login_gj_account_test() {
+    dotenv::from_filename("test_env.env").expect("test_env.env file not found");
+
+    let user_name = dotenv::var("GJ_ACCOUNT_USERNAME").unwrap();
+    let password = dotenv::var("GJ_ACCOUNT_PASSWORD").unwrap();
+
     let request = request::account::LoginRequest::default()
-        .user_name("Ryder")
-        .password("PASS_HERE");
+        .user_name(&user_name)
+        .password(&password);
 
     let result = request.to_authenticated_user().await.unwrap();
     println!("{:?}", result);
