f4200e4fda49e82457aa9a51134b8f219fe292b1
yuptune <106561594+yuptune@users.noreply.github.com>
2025-01-11 03:35:56 -0800

    Change mod.rs to use OnceCell for a more convenient and dynamic way to edit URLs for GDPSes
    The const was changed to a static of type ``OnceCell<&'static str>``.
    ``get_gd_url`` is now a call to ``OnceCell::get_or_init``, and the "or init" part is also set to the boomlings servers. (This is changeable, by the way. ;)). Also, i don't even know what i'm doing. So if i'm wrong please correct the mistakes i made in this commit.
    
    Hope this helps,
    yuptune

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,110 +1,41 @@
-//! Module containing all structs modelling requests to the boomlings APIs.
+//! Module containing all structs modelling requests to the Boomlings APIs.
 //!
 //! These directly implement (de)serialization into RobTop's data format, unlike models where
 //! RobTop's eccentricities are hidden. This is since directly re-using these structs outside of
 //! making/proxying requests for the boomlings servers seems rather useless to me, as they already
 //! contain a lot of boomlings-specific fields.
+//! This can also be edited for a specific GDPS, e.g 1.9 GDPS. (hi absowute :3)
 
 use crate::{model::GameVersion, serde::RequestSerializer};
 use serde::{Deserialize, Serialize};
+use once_cell::sync::OnceCell;
 
-macro_rules! const_setter {
-    ($name: ident, $field: ident, $t: ty) => {
-        pub const fn $name(mut self, $field: $t) -> Self {
-            self.$field = $field;
-            self
-        }
-    };
-
-    ($name: ident, $t: ty) => {
-        pub const fn $name(mut self, arg0: $t) -> Self {
-            self.$name = arg0;
-            self
-        }
-    };
-
-    ($(#[$attr:meta])* $name: ident: $t: ty) => {
-        $(#[$attr])*
-        pub const fn $name(mut self, $name: $t) -> Self {
-            self.$name = $name;
-            self
-        }
-    };
-
-    ($(#[$attr:meta])* $field:ident[$name: ident]: $t: ty) => {
-        $(#[$attr])*
-        pub const fn $name(mut self, $field: $t) -> Self {
-            self.$field = $field;
-            self
-        }
-    }
-}
-
-pub mod comment;
-pub mod level;
-pub mod user;
+static GDPS_URL: OnceCell<String> = OnceCell::new();
 
 pub fn get_gdps_url() -> String {
-    println!("ok1");
-    println!("{}",std::env::var("GDPS_URL").unwrap_or("https://www.boomlings.com/database/".to_string()).to_string());
-    std::env::var("GDPS_URL").unwrap_or("https://www.boomlings.com/database/".to_string())
+    GDPS_URL.get_or_init(|| std::env::var("GDPS_URL").unwrap_or_else(|_| "https://www.boomlings.com/database/".to_string()))
 }
 
-//pub const REQUEST_BASE_URL: &str = "https://www.boomlings.com/database/";
-
-/// A `BaseRequest` instance that has all its fields set to the
-/// same values a Geometry Dash 2.1 client would use
 pub const GD_21: BaseRequest = BaseRequest::new(
     GameVersion::Version { major: 2, minor: 1 },
     GameVersion::Version { major: 3, minor: 3 },
     "Wmfd2893gb7",
 );
 
-/// A `BaseRequest` instance that has all its fields set to the
-/// same values a Geometry Dash 2.2 client would use
 pub const GD_22: BaseRequest = BaseRequest::new(
     GameVersion::Version { major: 2, minor: 2 },
     GameVersion::Version { major: 3, minor: 8 },
     "Wmfd2893gb7",
 );
 
-/// Base data included in every request made
-///
-/// The fields in this struct are only relevant when making a request to the
-/// `boomlings` servers. When using GDCF with a custom Geometry Dash API, they
-/// can safely be ignored.
 #[derive(Debug, Clone, Hash, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct BaseRequest<'a> {
-    /// The version of the game client we're pretending to be
-    ///
-    /// ## GD Internals:
-    /// This field is called `gameVersion` in the boomlings API and needs to be
-    /// converted to a string response
-    /// The value of this field doesn't matter, and the request will succeed
-    /// regardless of what it's been set to
-    #[serde(rename = "gameVersion")]
     pub game_version: GameVersion,
-
-    /// Internal version of the game client we're pretending to be
-    ///
-    /// ## GD Internals:
-    /// This field is called `binaryVersion` in the boomlings API and needs to
-    /// be converted to a string
-    ///
-    /// The value of this field doesn't matter, and the request will succeed
-    /// regardless of what it's been set to
-    #[serde(rename = "binaryVersion")]
     pub binary_version: GameVersion,
-
-    /// The current secret String the server uses to identify valid clients.
-    ///
-    /// ## GD Internals:
-    /// Settings this field to an incorrect value will cause the request to fail
     pub secret: &'a str,
 }
 
 impl BaseRequest<'_> {
-    /// Constructs a new `BaseRequest` with the given values.
     pub const fn new(game_version: GameVersion, binary_version: GameVersion, secret: &'static str) -> BaseRequest<'static> {
         BaseRequest {
             game_version,
